1a1915de1ea0b4d4cc9d3942690a4dff2f28325b
Diego <diego35.da@gmail.com>
2021-08-09 08:55:32 -0500

    sum pairs improvement but still not done

--- CodeWars/5 kyu/sum of pairs.ts
+++ CodeWars/5 kyu/sum of pairs.ts
@@ -1,13 +1,9 @@
-// timeout no finished
+// timeout not finished
 export function sumPairs(ints: number[], s: number): [number, number] | void {  
-    const pairs: any = []
-    for (let i in ints) {
-        if (ints.indexOf(s - ints[i], +i + 1) !== -1) {
-            pairs.push([+i, ints.indexOf(s - ints[i], +i + 1)])
-            break
-        }
-    }
-    const sorted = [...pairs.sort((a, b) => a[1] - b[1])]
-    console.log(sorted)
+    const sorted = ints.map((val, i) => [i, ints.indexOf(s - val, i + 1)])
+        .filter((val, i) => val[1] !== -1)
+        .sort((a, b) => a[1] - b[1])
+    
+    console.log(ints)
     return sorted && sorted[0] && [ints[sorted[0][0]], ints[sorted[0][1]]]
   }
