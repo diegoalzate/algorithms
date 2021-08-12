acca1ea8db194911d875101b23da0c09a46bea27
Diego <diego35.da@gmail.com>
2021-08-11 23:25:04 -0500

    best answer for sum of pairs

--- CodeWars/5 kyu/sum of pairs.ts
+++ CodeWars/5 kyu/sum of pairs.ts
@@ -1,9 +1,16 @@
-// timeout not finished
 export function sumPairs(ints: number[], s: number): [number, number] | void {  
     const sorted = ints.map((val, i) => [i, ints.indexOf(s - val, i + 1)])
         .filter((val, i) => val[1] !== -1)
         .sort((a, b) => a[1] - b[1])
-    
-    console.log(ints)
+
     return sorted && sorted[0] && [ints[sorted[0][0]], ints[sorted[0][1]]]
   }
+
+  // best answer
+//   export function sumPairs(ints: number[], s: number): [number, number] | void {  
+//     const seen = new Set();
+//     for (const n of ints) {
+//       if (seen.has(s - n)) return [s - n, n];
+//       seen.add(n);
+//     }
+//   }
