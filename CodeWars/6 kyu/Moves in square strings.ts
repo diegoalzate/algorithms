a7416819689ea14253d71b07bae580eaf7295d72
Diego <diego35.da@gmail.com>
2021-08-14 12:39:03 -0500

    Moves in square

--- CodeWars/6 kyu/Moves in square strings.ts
+++ CodeWars/6 kyu/Moves in square strings.ts
@@ -1,29 +1,9 @@
 export function rot90Clock(strng: string): string {
-    const lines = strng.split('\n')
-    const response = []
-    for (let j in lines) {
-        let temp = ''
-        for (let i in [...lines[j]]) {
-            temp += lines[Math.abs(Number(i) - (lines[j].length - 1))][j]
-        }
-        response.push(temp)
-    }
-    console.log(response)
-    return response.join('\n')
+    return cycleOverLetters(strng, (i, j, lines) => lines[Math.abs(Number(i) - (lines[j].length - 1))][j])
 }
 
 export function diag1Sym(strng: string): string {
-    const lines = strng.split('\n')
-    const response = []
-    for (let j in lines) {
-        let temp = ''
-        for (let i in [...lines[j]]) {
-            temp += lines[Math.abs(Number(i))][j]
-        }
-        response.push(temp)
-    }
-    console.log(response)
-    return response.join('\n')
+    return cycleOverLetters(strng, (i,j,lines) => lines[Math.abs(Number(i))][j])
 }
 
 export function selfieAndDiag1(strng: string): string {
@@ -41,3 +21,15 @@ export function oper(fct: (s: string) => string, s: string): string {
     return fct(s)
 }
 
+export function cycleOverLetters(strng: string, callback: (i: string, j: string, arr: string[]) => string) {
+    const lines = strng.split('\n')
+    const response = []
+    for (let j in lines) {
+        let temp = ''
+        for (let i in [...lines[j]]) {
+            temp += callback(i,j,lines)
+        }
+        response.push(temp)
+    }
+    return response.join('\n')
+}
