922385d01b7daf0c25579177b64d3582c2f6bf8b
Diego <diego35.da@gmail.com>
2021-08-17 20:15:39 -0500

    start genetic algorithm

---
+++ CodeWars/6 kyu/Binary Genetic Algorithms.ts
@@ -0,0 +1,22 @@
+export class GeneticAlgorithm {
+    generate(length: number) {
+      // TODO: Implement the generate method
+    }
+  
+    select(population: string[], fitnesses: number[]) {
+      // TODO: Implement the select method
+    }
+  
+    mutate(chromosome: string, p: number) {
+      // TODO: Implement the mutate method
+    }
+  
+    crossover(chromosome1: string, chromosome2: string) {
+      // TODO: Implement the crossover method
+    }
+  
+    run(fitness: (chromosome: string) => number, length: number, p_c: number, p_m: number, iterations = 100) {
+      // TODO: Implement the run method
+    }
+  }
+  
